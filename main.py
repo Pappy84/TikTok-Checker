857bfca674b3b160f9004a6db7a3a5e320baf248
Pappy84 <134893391+Pappy84@users.noreply.github.com>
2023-12-18 12:07:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class Checker:
                     'accept'            : '*/*',
                     'accept-language'   : 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
                     'cookie'            : f'tiktok_webapp_theme=light; sessionid={choice(this.sessions)}',
-                    'referer'           : f'https://www.tiktok.com/@{unique_id}',
+                    'referer'           : f'https://www.tiktok.com/@dannydannyboyimbe}',
                     'sec-ch-ua'         : '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
                     'sec-ch-ua-mobile'  : '?0',
                     'sec-ch-ua-platform': '"Windows"',
